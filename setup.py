1d6c875be010deb9d9fec138e9aa578d93cb864d
Attila Gombos <attila.gombos@effective-range.com>
2025-05-19 12:59:24 +0200

    Update versioning and CI

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='example-python',
-    version='1.0.2',
     description='Example Python project',
     long_description='Example showcasing the dev tooling for Python based development',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
     packages=['example'],
     scripts=['bin/example-python.py'],
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     install_requires=['netifaces',
                       'python-context-logger@git+https://github.com/EffectiveRange/python-context-logger.git@latest']
 )
